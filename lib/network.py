592c53f5d8a87dafb3866ba000283cacf7b16524
Nathanael Coonrod <ncoonrod@highersoftware.com>
2018-05-01 00:05:35 -0700

    Fixes and updates

--- lib/network.py
+++ lib/network.py
@@ -140,7 +140,6 @@ def deserialize_proxy(s):
 
 
 def deserialize_server(server_str):
-    print ("STRTEST: {}".format (str(server_str)))
     host, port, protocol = str(server_str).rsplit(':', 2)
     assert protocol in 'st'
     int(port)    # Throw if cannot be converted to int
